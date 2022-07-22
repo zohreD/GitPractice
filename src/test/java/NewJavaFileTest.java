0df20cbbc69e08c49d7cf209bb70dd4b17850e43
ZohreD <84271729+zohreD@users.noreply.github.com>
2022-07-22 13:01:27 +0700

    second commit from local repo

---
+++ src/test/java/NewJavaFileTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class NewJavaFileTest extends PracticeClass {
+
+    @org.testng.annotations.BeforeMethod
+    public void setUp() {
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testControlTest() {
+
+
+    }
+}
--- src/test/java/PracticeClass.java
+++ src/test/java/PracticeClass.java
@@ -1,6 +1,9 @@
 public class PracticeClass {
     public static void main(String[] args) {
         System.out.println("Practicing Git and GitHub");
+
+
+        System.out.println("there are some changes made");
     }
 
 
