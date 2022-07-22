2d056885a33c165afac2543f206149af1eeda479
ZohreD <84271729+zohreD@users.noreply.github.com>
2022-07-22 13:08:14 +0700

    for loop is added

--- src/test/java/NewJavaFile.java
+++ src/test/java/NewJavaFile.java
@@ -1,6 +1,11 @@
 public class NewJavaFile {
 
-
+    public static void main(String[] args) {
+        String str = "level";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.substring(i));
+        }
+    }
 
 
 }
